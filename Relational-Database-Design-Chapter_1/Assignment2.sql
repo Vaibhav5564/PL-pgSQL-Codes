53e9c9e2c8cb8f19c386e7a03b4b87459d1c2fdb
Vaibhav Rajendra Adsul <vaibhavadsul5564@gmail.com>
2026-03-06 10:05:35 +0000

    Assignment-2-Complete

--- Relational-Database-Design-Chapter_1/Assignment2.sql
+++ Relational-Database-Design-Chapter_1/Assignment2.sql
@@ -214,3 +214,356 @@ Using If.-Then-else,case,for,while and unconditional loops
 -- language 'plpgsql';
 
 -- select printN(123);
+
+/*
+Consider the following database maintained by a Bank. The Bank maintains information about its 
+branches, customers and their loan applications.
+Following are the tables:
+branch (bid integer, brname char (30), brcity char (10))
+customer (cno integer, cname char (20), caddr char (35), city char(20)) 
+loan_application (lno integer, lamtrequired money, lamtapproved money, l_date date)
+The relationship is as follows: branch, customer, loan_application are related with ternary 
+relationship. TERNARY (bid integer, cno integer, lno integer).
+*/
+
+--                                            Set B
+
+-- create table branch(bid int primary key, bname varchar(20), bcity varchar(10));
+-- create table customer(cno int primary key, cname varchar(20), caddr varchar(40), city varchar(10));
+-- create table loanApplication(lno int primary key, lrequired int, lapproved int, ldate date);
+-- create table brCustLoan(bid int references branch(bid), cno int references customer(cno), lno int references loanApplication(lno));
+
+-- insert into branch values(1, 'BOM', 'Taradgaon');
+-- insert into branch values(2, 'BOI', 'Lonand');
+-- insert into branch values(3, 'BOB', 'Pune');
+-- insert into branch values(4, 'ICICI', 'Mumbai');
+
+-- insert into customer values(101, 'Satyam', 'Malewadi', 'Lonand');
+-- insert into customer values(102, 'Saurabh', 'Malewadi', 'Pune');
+-- insert into customer values(103, 'Sushant', 'Tardgaon', 'Phaltan');
+-- insert into customer values(104, 'Mrunal', 'Tardgaon', 'Mumbai');
+
+-- insert into loanApplication values(1001, 100000, 100000, '3/6/2006');
+-- insert into loanApplication values(1002, 1000000, 1000000, '3/6/2026');
+-- insert into loanApplication values(1003, 1000, 1000, '3/4/2016');
+-- insert into loanApplication values(1004, 10000000, 10000000, '3/6/2026');
+
+-- insert into brCustLoan values(1, 101, 1001);
+-- insert into brCustLoan values(2, 103, 1004);
+-- insert into brCustLoan values(3, 104, 1004);
+-- insert into brCustLoan values(1, 104, 1002);
+
+/*
+a) Write a function that returns the total number of customers of a particular branch.(Accept 
+branch name as input parameter.)
+b) Write a function to find the minimum loan amount approved.
+c) Create a function which returns the total number of customers who have applied for a loan 
+more than Rs.200000. (Accept loan amount as input parameter)
+*/
+
+-- a)
+-- create or replace function totalCustomer(name varchar(20)) returns int as 
+-- $$
+-- declare
+-- cnt int := 0;
+-- fbno int;
+-- begin
+-- select bid into fbno from branch where name = bname;
+-- select count(*) into cnt from brCustLoan group by bid having bid = fbno;
+-- return cnt;
+-- end;
+-- $$
+-- language 'plpgsql';
+
+-- select totalCustomer('BOM');
+
+
+-- b)
+-- create or replace function minLoan() returns int as
+-- $$
+-- declare
+-- res int ;
+-- begin
+-- select min(lapproved) into res from loanApplication;
+-- return res;
+-- end
+-- $$
+-- language 'plpgsql';
+
+-- select minLoan();
+
+
+-- c)
+-- create or replace function custLoan(amt int) returns int as
+-- $$
+-- declare
+-- res int ;
+-- begin
+-- select count(*) into res from loanApplication where lrequired > amt;
+-- return res;
+-- end
+-- $$
+-- language 'plpgsql';
+
+-- select custLoan(200000);
+
+
+/*
+
+--                                            Set c
+
+Student- Teacher database 
+Consider the following database maintained by a school. The school maintains information about 
+students and the teachers. It also gives information of the subject taught by the teacher.
+Following are the tables:
+student (sno integer, s_name char(30), s_class char(10), s_addr char(50))
+teacher (tno integer, t_name char (20), qualification char (15), experience integer)
+The relationship is as follows: STUDENT-TEACHER: Many to Many with descriptive attribute 
+SUBJECT.
+*/
+
+-- create table student (sno integer primary key, sname varchar(30), sclass varchar(10), saddr varchar(50));
+-- create table teacher (tno integer primary key, tname varchar (20), qualification varchar (15), experience integer);
+-- create table Stud_Teach(sno int references student(sno), tno int references teacher(tno), subject varchar(20)); 
+
+
+-- insert into student values(1, 'Vaibhav', 'SY', 'Pune');
+-- insert into student values(2, 'Sushant', 'SY', 'Phaltan');
+-- insert into student values(3, 'Rohit', 'FY', 'Mumbai');
+-- insert into student values(4, 'Mayur', 'TY', 'Satara');
+
+-- insert into teacher values(101, 'Mrs Deepashree', 'BCS', 20);
+-- insert into teacher values(201, 'Mrs Shubhangi', 'MSC', 20);
+-- insert into teacher values(301, 'Mr Parameshwar', '12th', 2);
+-- insert into teacher values(401, 'Mrs Neha', 'MSC', 10);
+
+-- insert into Stud_Teach values(1, 201, 'DSA');
+-- insert into Stud_Teach values(2, 301, 'Share Market');
+-- insert into Stud_Teach values(3, 401, 'Mathematics');
+-- insert into Stud_Teach values(1, 101, 'Computer');
+
+/*
+a) Write a function to find name of the most experienced teacher for “Computer”.
+b) Write a function to find the teacher teaching maximum number of subjects.
+c) Write a function to find the number of teachers having qualification “MSC”
+*/
+
+-- a)
+-- create or replace function mostExp(sub varchar(20)) returns varchar(20) as 
+-- $$
+-- declare 
+-- res varchar(20);
+-- maxres int;
+-- begin
+-- SELECT t.tname INTO res FROM teacher t JOIN Stud_Teach s ON t.tno = s.tno WHERE s.subject = sub ORDER BY t.experience DESC LIMIT 1;
+-- return res;
+-- end;
+-- $$
+-- language 'plpgsql';
+
+-- select mostExp('Computer');
+
+
+-- b)
+-- create or replace function maxSub() returns varchar(20) as
+-- $$
+-- declare 
+-- id int;
+-- name varchar(20);
+-- begin
+-- select tno into id from Stud_Teach group by tno order by count(*) desc limit 1;
+-- select tname into name from teacher where tno = id;
+-- return name;
+-- end;
+-- $$
+-- language 'plpgsql';
+
+-- select maxSub();
+
+
+-- c)
+-- create or replace function qualNo(qual varchar(10)) returns int as
+-- $$
+-- declare 
+-- cnt int;
+-- begin
+-- select count(*) into cnt from teacher where qualification = qual; 
+-- return cnt;
+-- end;
+-- $$
+-- language 'plpgsql';
+
+-- select qualNo('BCS');
+
+
+--                                            Set D
+
+/*
+Project – Employee Database 
+Consider the following database
+Project (pno int, pname char (30), ptype char (20), duration int) 
+Employee (empno int, ename char (20), joining_date date)
+The relationship between Project and Employee is many to many with descriptive attribute start_date.
+Create the above database in PostGreSQL and insert sufficient records.
+Execute any two of the following using PL/pgSQL
+*/
+
+-- create table Project(pno int primary key, pname char(30), ptype char(20), duration int); 
+-- create table Employee(empno int primary key, ename char (20), joining_date date);
+-- create table Proj_Emp(pno int references Project(pno), empno int references Employee(empno), sdate date);
+
+-- insert into Project values(101, 'Solar', 'Hardware', 2);
+-- insert into Project values(102, 'Calculator', 'Software', 1);
+-- insert into Project values(103, 'Password Cracker', 'Software', 5);
+
+-- insert into Employee values(1, 'Vaibhav', '04/18/2006');
+-- insert into Employee values(2, 'Satyam', '11/10/2005');
+-- insert into Employee values(3, 'Sushant', '11/29/2005');
+
+-- insert into Proj_Emp values(103, 1, '04/18/2026');
+-- insert into Proj_Emp values(102, 2, '04/14/2016');
+-- insert into Proj_Emp values(101, 3, '04/18/2026');
+
+
+/*
+a. Write a stored function to accept project type as an input and display all project names of 
+that type.
+b. Write a function which accepts employee name and prints the details of the project which 
+the employee works on.
+c. Write a function to accept project name as input and returns the number of employees 
+working on the project
+*/
+
+-- a)
+-- create or replace function displayProject(projectype varchar(20)) returns text as
+-- $$
+-- declare
+-- txt text := ' ';
+-- itr project%rowtype;
+-- begin
+-- for itr in select * from Project where ptype = projectype loop
+-- txt := txt||itr.pname||e'\n';
+-- end loop;
+-- return txt;
+-- end;
+-- $$
+-- language 'plpgsql';
+
+-- select displayProject('Software');
+
+
+-- b)
+-- create or replace function empDetail(name varchar(20)) returns Project as
+-- $$
+-- declare
+-- info Project%rowtype;
+-- begin
+-- select * into info from Project where pno = (select pno from Proj_Emp pe join Employee e on pe.empno = e.empno where e.ename = name);
+-- return info;
+-- end;
+-- $$
+-- language 'plpgsql';
+
+-- select empDetail('Vaibhav');
+
+
+-- c)
+-- create or replace function countEmp(name varchar(20)) returns int as
+-- $$
+-- declare 
+-- cnt int;
+-- begin
+-- select count(*) into cnt from Proj_Emp pe join Project p on p.pno = pe.pno where p.pname = name;
+-- return cnt;
+-- end;
+-- $$
+-- language 'plpgsql';
+
+-- select countEmp('Solar');
+
+
+--                                            Set E
+
+/*
+Student - Subject Database 
+Consider the following database
+Student (roll_no integer, name varchar(30), address varchar(50), class varchar(10)) Subject 
+(scode varchar(10), subject_name varchar(20))
+Student-Subject are related with Many to Many relationship with attributes
+marks_scored. Create the above database in PostGreSQL and insert sufficient records.
+*/
+
+-- create table Student(rno integer primary key, sname varchar(30), address varchar(50), class varchar(10));
+-- create table Subject(scode varchar(10) primary key, subname varchar(20));
+-- create table Sub_Stud(rno int references Student(rno), scode varchar(20) references Subject(scode), marks int);
+
+-- insert into student values(1, 'Siddharth', 'Pimpri', 'SY');
+-- insert into student values(2, 'Vaibhav', 'Pune', 'SY');
+-- insert into student values(3, 'Kalyani', 'Mumbai', 'SY');
+
+-- insert into Subject values('Eng', 'English');
+-- insert into Subject values('Mat', 'Mathematics');
+-- insert into Subject values('Phy', 'Physics');
+
+-- insert into Sub_Stud values(1, 'Eng', 40);
+-- insert into Sub_Stud values(2, 'Mat', 100);
+-- insert into Sub_Stud values(3, 'Phy', 50);
+
+-- a.Write a function which will accept the name and print all the details of that student.
+-- b. Write a function to accept student roll_no as input and displays details of that student.
+-- c.Write a stored function using cursors, to accept class from the user and display the details of the 
+-- students of that class.
+
+-- a)
+-- create or replace function studetails(name varchar(10))
+-- returns record as
+-- $$
+-- declare
+-- rec record;
+-- begin
+-- select s.*, ss.marks, sub.subname into rec from student s join sub_stud ss on s.rno = ss.rno join subject sub on sub.scode = ss.scode where s.sname = name;
+-- return rec;
+-- end;
+-- $$
+-- language plpgsql;
+
+-- select studetails('Vaibhav');
+
+
+-- b)
+-- create or replace function studetails(n int)
+-- returns record as
+-- $$
+-- declare
+-- rec record;
+-- begin
+-- select s.*, ss.marks, sub.subname into rec from student s join sub_stud ss on s.rno = ss.rno join subject sub on sub.scode = ss.scode where s.rno = n;
+-- return rec;
+-- end;
+-- $$
+-- language plpgsql;
+
+-- select studetails(2);
+
+
+-- c)
+-- create or replace function displayDetails(studClass varchar(10)) returns text as
+-- $$
+-- declare 
+-- rec record;
+-- txt text := '';
+-- cur cursor for select s.*, sub.subname, ss.marks from Student s join Sub_Stud ss on s.rno = ss.rno join Subject sub on ss.scode = sub.scode where s.class = studClass;
+-- begin
+-- open cur;
+-- loop
+-- fetch cur into rec;
+-- exit when not found;
+-- txt := txt||rec.rno||' '||rec.sname||' '||rec.address||' '||rec.subname||' '||rec.marks||e'\n';
+-- end loop;
+-- close cur;
+-- return txt;
+-- end;
+-- $$
+-- language 'plpgsql';
+
+-- select displayDetails('SY');
